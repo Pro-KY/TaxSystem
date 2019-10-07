3fd71be12f350b03cf3cb7cf654ee607b88b2ea5
Yuriy <klyuyko.yu@gmail.com>
2019-10-08 02:11:55 +0300

    refactor EmptyCommand

--- src/main/java/ua/training/command/impl/EmptyCommand.java
+++ src/main/java/ua/training/command/impl/EmptyCommand.java
@@ -1,14 +1,24 @@
 package ua.training.command.impl;
 
 import ua.training.command.ICommand;
+import ua.training.util.constans.Attributes;
 import ua.training.util.handler.properties.ViewPropertiesHandler;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 
 public class EmptyCommand implements ICommand {
     @Override
     public String execute(HttpServletRequest request) {
-        return ViewPropertiesHandler.getViewPath(ViewPropertiesHandler.PATH_INDEX);
+
+        final HttpSession session = request.getSession(false);
+        boolean isAuthorized = false;
+        if (session != null) {
+            isAuthorized = (boolean) session.getAttribute(Attributes.IS_USER_AUTHORIZED);
+        }
+
+        String property = isAuthorized ? ViewPropertiesHandler.PATH_MAIN : ViewPropertiesHandler.PATH_INDEX;
+        return ViewPropertiesHandler.getViewPath(property);
     }
 }
