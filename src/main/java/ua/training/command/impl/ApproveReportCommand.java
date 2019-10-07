4fa245fb0cc8b855938522bdaec64c2b97788e90
Yuriy <klyuyko.yu@gmail.com>
2019-10-08 02:52:05 +0300

    refactor ApproveReportCommand

--- src/main/java/ua/training/command/impl/ApproveReportCommand.java
+++ src/main/java/ua/training/command/impl/ApproveReportCommand.java
@@ -3,15 +3,20 @@ package ua.training.command.impl;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import ua.training.command.ICommand;
+import ua.training.command.util.CommandUtil;
+import ua.training.dto.PaginationDto;
 import ua.training.service.ReportApprovalService;
+import ua.training.util.constans.Attributes;
 import ua.training.util.constans.Parameters;
 import ua.training.util.handler.properties.ViewPropertiesHandler;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
-import static ua.training.util.handler.properties.ViewPropertiesHandler.PATH_MAIN;
+import static ua.training.util.constans.Attributes.ALERT_ERROR;
+import static ua.training.util.constans.Attributes.ALERT_SUCCESS;
+import static ua.training.util.handler.properties.ViewPropertiesHandler.*;
 
-// for inspector
 public class ApproveReportCommand implements ICommand {
     private static final Logger log = LogManager.getLogger(ApproveReportCommand.class);
 
@@ -21,9 +26,24 @@ public class ApproveReportCommand implements ICommand {
         final Long reportApprovalId = Long.valueOf(request.getParameter(Parameters.REPORT_APPROVAL_ID));
         final String refusalCause = request.getParameter(Parameters.REFUSAL_CAUSE);
         final Long stateApprovalId = Long.valueOf(request.getParameter(Parameters.REPORTS_APPROVAL_TYPE));
+        final HttpSession session = request.getSession();
+        final PaginationDto currentPaginationDto = CommandUtil.getInstance().getCurrentPaginationDto(session);
 
-        ReportApprovalService.getInstance().updateReportApproval(reportApprovalId, refusalCause, stateApprovalId);
+        boolean isOperationSuccessful;
 
+        try {
+            ReportApprovalService.getInstance().updateReportApproval(reportApprovalId, refusalCause, stateApprovalId);
+            isOperationSuccessful = true;
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            isOperationSuccessful = false;
+        }
+
+        String attributeName = isOperationSuccessful ? ALERT_SUCCESS : ALERT_ERROR;
+        request.setAttribute(attributeName, true);
+
+        session.setAttribute(Attributes.PAGINATION_INFO, currentPaginationDto);
+        request.setAttribute(Attributes.FRAGMENT_PATH, getViewPath(FRAGMENT_PATH_SENT_REPORTS));
         return ViewPropertiesHandler.getViewPath(PATH_MAIN);
     }
 }
